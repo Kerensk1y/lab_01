e034a6d73bc72cac381bff00965567f8ab80d507
RomanovKR <73065648+Kerensk1y@users.noreply.github.com>
2021-06-09 19:52:14 +0300

    Update lab 01.cpp
    specific of variant 18 taken into account

--- lab 01/lab 01.cpp
+++ lab 01/lab 01.cpp
@@ -1,106 +1,117 @@
-﻿#include <iostream>
-#include <vector>
-using namespace std;
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main()
 {
-	const size_t SCREEN_WIDTH = 80;
-	const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 1;
-	setlocale(LC_ALL, "RUS");
+    const size_t SCREEN_WIDTH = 80;
+    size_t number_count;
+    cerr << "Enter number count: ";
+    cin >> number_count;
+    vector<double> numbers(number_count);
+    cerr << "Enter numbers: ";
+    for (int i = 0; i < number_count; i++)
+    {
+        cin >> numbers[i];
+    }
+    size_t bin_count;
+    cerr << "Enter bins count: ";
+    cin >> bin_count;
+    cerr << "+------------------+" << endl;
 
-	size_t number_count;
-	cerr << "Введите количество чилел:\t"; cin >> number_count;
+    double min = numbers[0];
+    double max = numbers[0];
+    for (double x : numbers)
+    {
+        if (x < min)
+        {
+            min = x;
+        }
+        else if (x > max)
+        {
+            max = x;
+        }
+    }
+    vector<size_t> bins(bin_count);
+    double bin_size = (max - min) / bin_count;
+    for (int i = 0; i < number_count; i++) {
+        bool found = false;
+        for (int j = 0; (j < bin_count - 1) && !found; j++) {
+            auto lo = min + j * bin_size;
+            auto hi = min + (j + 1) * bin_size;
+            if ((lo <= numbers[i]) && (numbers[i] < hi)) {
+                bins[j]++;
+                found = true;
+            }
+        }
+        if (!found) {
+            bins[bin_count - 1]++;
+        }
+    }
 
-	vector<double> numbers(number_count);
-	for (size_t i = 0; i < number_count; i++)
-	{
-		cerr << "Введите " << i + 1 << " число:\t"; cin >> numbers[i];
-	}
+    size_t count;
+    size_t max_count = bins[0];
+    for (int i = 1; i < bin_count; i++) {
+        if (bins[i] > max_count) {
+            max_count = bins[i];
+        }
+    }
 
-	size_t bin_count;
-	cerr << "Введите количество групп:\t"; cin >> bin_count;
+    bool flag = false;
+    int n = 1;
+    while (!flag)
+    {
+        if (max_count / 10 >= 1)
+        {
+            n++;
+            max_count = max_count / 10;
+        }
+        else
+            flag = true;
+    }
 
-	double min = numbers[0];
-	double max = numbers[0];
-	for (size_t i = 0; i < number_count; i++)
-	{
-		if (numbers[i] < min)
-		{
-			min = numbers[i];
-		}
-		if (numbers[i] > max)
-		{
-			max = numbers[i];
-		}
-	}
+    const size_t MAX_ASTERISK = SCREEN_WIDTH - n - 1;
 
-	double bin_size = (max - min) / bin_count;
+    if (max_count > MAX_ASTERISK) {
+        for (int i = 0; i < bin_count; i++) {
+            count = bins[i];
+            size_t height = MAX_ASTERISK * (static_cast<double>(count) / max_count);
+            size_t r = max_count;
+            for (size_t i = 0; i < n; i++)
+            {
 
-	vector<size_t> bins(bin_count);
+                if (bins[i] / r == 0)
+                {
+                    cout << " ";
+                }
+                else
+                {
+                    cout << bins[i];
+                    break;
+                }
+                r = r / 10;
+
+            }
+            for (int j = 0; j < height; j++) {
+                cout << '*';
+            }
+            cout << endl;
+        }
+    }
+    else {
+        for (int i = 0; i < bin_count; i++) {
+            if (bins[i] < 100) cout << ' ';
+            if (bins[i] < 10) cout << ' ';
+            cout << bins[i] << '|';
+            for (int j = 0; j < bins[i]; j++) {
+                cout << '*';
+            }
+            cout << endl;
+        }
+    }
 
 
-	for (size_t i = 0; i < number_count; i++)
-	{
-		bool found = false;
-		for (size_t j = 0; (j < bin_count - 1) && !found; j++)
-		{
-			auto low = min + j * bin_size;
-			auto high = min + (j + 1) * bin_size;
-			if ((numbers[i] >= low) && (numbers[i] < high))
-			{
-				bins[j]++;
-				found = true;
-			}
-		}
-		if (!found)
-		{
-			bins[bin_count - 1]++;
-		}
-	}
-
-	size_t max_count = bins[0];
-
-	for (size_t i = 0; i < bin_count; i++)
-	{
-		if (bins[i] > max_count)
-		{
-			max_count = bins[i];
-		}
-	}
-	if (max_count > MAX_ASTERISK)
-	{
-
-		size_t height;
-		for (size_t i = 0; i < bin_count; i++)
-		{
-			height = MAX_ASTERISK * (static_cast<double>(bins[i]) / max_count);
-			if (bins[i] < 100)
-				cout << " ";
-			if (bins[i] < 10)
-				cout << " ";
-			cout << bins[i] << "|";
-			for (size_t j = 0; j < height; j++)
-			{
-				cout << "*";
-			}
-			cout << endl;
-		}
-	}
-	else
-	{
-		for (size_t i = 0; i < bin_count; i++)
-		{
-			if (bins[i] < 100)
-				cout << " ";
-			if (bins[i] < 10)
-				cout << " ";
-			cout << bins[i] << "|";
-			for (size_t j = 0; j < bins[i]; j++)
-			{
-				cout << "*";
-			}
-			cout << endl;
-		}
-	}
+    return 0;
 }
